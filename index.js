02f988ea08a6f6e943c6f0dd7bcf32fdfbd5837f
Mariz Melo <mm@xchema.com>
2013-04-04 16:27:32 -0700

    creating output

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ program
   .parse(process.argv);
 
 // check if directory was specified
-if (program.args.length) {
-  
-}//if
+if (program.args.length) {}//if
+
+
+// check if user specified directory
+if (program.args[0]) {
+  markalldown.mark.model.dir = program.args[0];
+}
+
+// check if should create output file
+if (program.output) {
+  markalldown.mark.model.output = true;
+}
+
+// execute program
+markalldown.mark.init();

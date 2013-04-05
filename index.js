9253a8b62dbbaee2b897d1a7ab474e06d1188d59
Mariz Melo <mm@xchema.com>
2013-04-04 17:47:43 -0700

    version 0.0.1

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ program
   .usage('[directory]')
   .parse(process.argv);
 
-// check if directory was specified
-if (program.args.length) {}//if
-
-
 // check if user specified directory
 if (program.args[0]) {
   markalldown.mark.model.dir = program.args[0];

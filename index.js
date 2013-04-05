b9cc5dde2b2dc0cfb313d598cabfb92f1e0b9a04
Mariz Melo <mm@xchema.com>
2013-04-05 11:48:30 -0700

    fix bug on version 0.0.2

--- index.js
+++ index.js
@@ -10,25 +10,25 @@
 // DEPENDENCIES
 var program = require('commander') // command line options
   , fs = require('fs')  // node fileSystem lib
-  , markalldown = require('./lib/markalldown') // markalldown lib
+  , $ = require('./lib/markalldown') // markalldown lib
 
 
 // commander options
 program
-  .version('0.0.1')
+  .version('0.0.2')
   .option('-o, --output', 'Create README.md on current directory')
   .usage('[directory]')
   .parse(process.argv);
 
 // check if user specified directory
 if (program.args[0]) {
-  markalldown.mdall.model.dir = program.args[0];
+  $.markalldown.model.dir = program.args[0];
 }
 
 // check if should create output file
 if (program.output) {
-  markalldown.mdall.model.output = true;
+  $.markalldown.model.output = true;
 }
 
 // execute program
-markalldown.mdall.init();
+$.markalldown.init();
